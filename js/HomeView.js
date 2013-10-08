fbc450f7f5cf7f92fefcbdd7530c6141b1e55419
Prasad Sangodkar <prasad0079@rediffmail.com>
2013-10-08 15:22:32 +0530

    updated..

--- js/HomeView.js
+++ js/HomeView.js
@@ -16,10 +16,23 @@ var HomeView = function(store){
         return this;
     };
 
-    this.findByName = function() {
+    /*this.findByName = function() {
         var self = this;
         store.findByName($('.search-key').val(), function(employees){
             $('.employee-list').html(HomeView.liTemplate(employees));
+        });
+    };*/
+
+    this.findByName = function() {
+        store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(HomeView.liTemplate(employees));
+            if (self.iscroll) {
+                console.log('Refresh iScroll');
+                self.iscroll.refresh();
+            } else {
+                console.log('New iScroll');
+                self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+            }
         });
     };
 
--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
-        //this.bodyInit();
+        this.bodyInit();
     },
     // Bind Event Listeners
     //
@@ -47,7 +47,28 @@ var app = {
 
         //this.homeTpl = Handlebars.compile($("#home-tpl").html());
         //this.employeeLiTpl = Handlebars.compile($("employee-li-tpl").html());
-        this.bodyInit();
+        //this.bodyInit();
+    },
+
+    registerEvents: function(){
+        var self = this;
+        if(document.documentElement.hasOwnProperty('ontouchstart')){
+            $('body').on('touchstart', 'a', function(event){
+              $(event.target).addClass('tappable-active');
+            });
+
+            $('body').on('touchend', 'a', function(event){
+                $(event.target).removeClass('tappable-active');
+            })
+        }else{
+            $('body').on('mousedown', 'a', function(event){
+                $(event.target).addClass('tappable-active');
+            });
+
+            $('body').on('mouseup','a',function(event){
+                $(event.target).removeClass('tappable-active');
+            });
+        }
     },
 
     /*findByName: function() {
@@ -86,6 +107,7 @@ var app = {
         this.store = new MemoryStore(function(){
             //self.showAlert('Store initialized', 'info');
             //self.renderHomeView();
+            self.registerEvents();
             $('body').html(new HomeView(self.store).render().el);
         });
 
