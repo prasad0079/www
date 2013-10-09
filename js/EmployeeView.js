0da7fa93fe6a9efd9b255a6c7bc0bfec3cffd11f
Prasad Sangodkar <prasad0079@rediffmail.com>
2013-10-09 14:13:49 +0530

    2.13pm

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -12,11 +12,37 @@ var EmployeeView = function(employee){
         alert("hhooo hooo")
         this.el.on('click', '.add-location-btn', this.addLocation);
         this.el.on('click', '.add-contact-btn', this.addToContacts);
+        this.el.on('click', '.change-pic-btn', this.changePicture);
     };
 
     this.render = function(){
         this.el.html(EmployeeView.template(employee));
         return this;
+    };
+
+    this.changePicture = function(event){
+        event.preventDefault();
+        if(!navigator.camera){
+            app.showAlert("Camera Not supported", "Error");
+            return;
+        }
+
+        var options = {     quality:50,
+                            destinationType: Camera.DestinationType.DATA_URL,
+                            sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+                            encodingType: 0     // 0=JPG 1=PNG
+                      };
+
+        navigator.camera.getPicture(
+            function(imageData){
+                $('.employee-image',this.el).attr('src', "data:image/jpeg;base64,"+imageData);
+            },
+            function(){
+                app.showAlert("Error taking picture", "Error");
+            },
+            options);
+
+        return false;
     };
 
     this.addLocation = function(event){
