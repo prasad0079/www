b50681fde7116e1997ee781feae0abee1742fc5a
Prasad Sangodkar <prasad0079@rediffmail.com>
2013-10-09 12:16:21 +0530

    12.16pm

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -9,7 +9,8 @@
 var EmployeeView = function(employee){
     this.initialize = function(){
         this.el = $('<div/>');
-        this.el.on('click', '.add-location-btn', this.addLocation)
+        this.el.on('click', '.add-location-btn', this.addLocation);
+        this.el.on('click', '.add-contact-btn', this.addToContacts);
     };
 
     this.render = function(){
@@ -28,6 +29,22 @@ var EmployeeView = function(employee){
                 alert("Error getting location");
             }
         );
+        return false;
+    };
+
+    this.addToContacts = function(event){
+        event.preventDefault();
+        if(!navigator.contacts){
+            app.showAlert("Contacts API not supported", "Error");
+            return;
+        }
+        var contact = navigator.contacts.create();
+        contact.name = {givenName:employee.firstName, familyName:employee.lastName};
+        var phoneNumbers = [];
+        phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+        phoneNumbers[1] = new ContactField('home', employee.cellPhone, true);
+        contact.phoneNumbers = phoneNumbers;
+        contact.save();
         return false;
     };
 
